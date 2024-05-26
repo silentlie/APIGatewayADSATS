fca021d8d2960f88bf63cf1b49142ac8253d35b5
unknown <52875747+silentlie@users.noreply.github.com>
2024-05-26 13:32:10 +1000

    add lambda function folder

---
+++ testAPI/get_method.py
@@ -0,0 +1,41 @@
+from mysql.connector import Error
+import mysql.connector
+import os
+
+def get_method(body):
+    try:
+        query = read_file("test.sql")
+        connection = mysql.connector.connect(
+            host= os.environ.get('HOST'),
+            user= os.environ.get('USER'),
+            password= os.environ.get('PASSWORD'),
+            database= "",
+        )
+        cursor = connection.cursor()
+        cursor.execute(query)
+        if query.strip().upper().startswith("SELECT"):
+            results = cursor.fetchall()
+            for row in results:
+                print(row)
+        else:
+            connection.commit()
+            print(f"Query executed successfully and changes committed: {query}")
+    except Error as e:
+        print(f"Error: {e}")
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+    return {
+        'statusCode': 200,
+        'headers': {
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+        'body': "Succed"
+    }
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
