8905cb7954a1f7684147e2323afea280250c0c42
unknown <52875747+silentlie@users.noreply.github.com>
2024-05-27 16:13:14 +1000

    test get method

--- documentsEndPoint/get_method.py
+++ documentsEndPoint/get_method.py
@@ -8,26 +8,61 @@ def get_method(parameters):
             host= os.environ.get('HOST'),
             user= os.environ.get('USER'),
             password= os.environ.get('PASSWORD'),
-            database= "",
+            database= "adsats_database",
         )
         cursor = connection.cursor()
-        name = parameters.get("name")
-        emails = parameters.get("emails")
-        timeRange = parameters.get("timeRange")
-        archived = parameters.get("archived")
-        aircrafts = parameters.get("aircrafts")
-        columnName = parameters.get("clumnName")
-        asc = parameters.get("asc")
-        query = ""
-
-        cursor.execute(query)
-        if query.strip().upper().startswith("SELECT"):
-            results = cursor.fetchall()
-            for row in results:
-                print(row)
-        else:
-            connection.commit()
-            print(f"Query executed successfully and changes committed: {query}")
+        name = parameters.get("name", default=None)
+        emails = parameters.get("emails", default=None).split(',')
+        timeRange = parameters.get("timeRange", default=None).split(',')
+        archived = parameters.get("archived", default=None)
+        aircrafts = parameters.get("aircrafts", default=None).split(',')
+        columnName = parameters.get("clumnName", default=None)
+        asc = parameters.get("asc", default=None)
+        limit = parameters.get("limit")
+        offset = parameters.get("offset")
+        query = """
+        SELECT d.document_id, d.file_name, u.email, d.archived, d.created_at, d.modified_at, ss.name, GROUP_CONCAT(a.name SEPARATOR ', ') 
+        FROM documents AS d
+        JOIN users AS u ON d.uploaded_by_id = u.user_id
+        JOIN subcategories AS ss ON ss.subcategory_id = d.subcategory_id
+        JOIN aircraft_documents AS ad ON ad.documents_id = d.document_id
+        JOIN aircrafts AS a ON ad.aircrafts_id = a.aircraft_id
+        """
+        conditions = []
+        params = []
+        if name is not None:
+            conditions.append("d.file_name = %s")
+            params.append(name)
+        if emails is not None:
+            placeholders = ', '.join(['%s'] * len(emails))
+            conditions.append(f"u.emails IN ({placeholders})")
+            params.extend(emails)
+        if timeRange is not None:
+            conditions.append("d.created_at BETWEEN %s AND %s")
+            params.extend(timeRange)
+        if archived is not None:
+            conditions.append("d.archived = %s")
+            params.append(archived)
+        if aircrafts is not None:
+            placeholders = ', '.join(['%s'] * len(aircrafts))
+            conditions.append(f"ad.aircraft_id IN ({placeholders})")
+        if conditions:
+            query += " WHERE " + " AND ".join(conditions)
+        if columnName is not None:
+            query += "ORDER BY d.%s %s"
+            params.append(columnName)
+            if asc:
+                params.append('ASC')
+            else:
+                params.append('DESC')
+        query += "LIMIT %s OFFSET %s"
+        params.extend([limit, offset])
+        
+        cursor.execute(query, params)
+        results = cursor.fetchall()
+        for row in results:
+            # need to convert to a list of dict/json
+            print(row)
     except Error as e:
         print(f"Error: {e}")
     finally:
@@ -42,5 +77,6 @@ def get_method(parameters):
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
             },
+        # this suppose to return all rows
         'body': "Succeed"
     }
