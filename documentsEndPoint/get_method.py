1f7e7a5cab43976ad79bb379946ff0fee3ba13c3
Tuan Linh Nguyen <52875747+silentlie@users.noreply.github.com>
2024-05-27 21:31:29 +1000

    finish error handling get method

--- documentsEndPoint/get_method.py
+++ documentsEndPoint/get_method.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 import json
 from mysql.connector import Error
 import mysql.connector
 import os
 
 def get_method(parameters):
+    error_message = ""
     try:
         connection = connect_to_db()
         cursor = connection.cursor()
@@ -17,13 +19,24 @@ def get_method(parameters):
             response.append(row)
             print(row)
     except Error as e:
-        print(f"Error: {e}")
+        print(f"Error: {e._full_msg}")
+        error_message = e._full_msg
     finally:
         if cursor is not None:
             cursor.close()
         if connection.is_connected():
             connection.close()
             print("MySQL connection is closed")
+    if error_message:
+        return {
+            'statusCode': 500,
+            'headers': {
+                    'Access-Control-Allow-Headers': '*',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,PATCH,DELETE'
+                },
+            'body': json.dumps(error_message)
+        }
     return {
         'statusCode': 200,
         'headers': {
@@ -31,17 +44,17 @@ def get_method(parameters):
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,PATCH,DELETE'
             },
-        'body': json.dumps(response, indent=4, separators=(',', ':'))
+        'body': json.dumps(response, indent=4, separators=(',', ':'), cls=DateTimeEncoder)
     }
 
 def build_query(parameters):
     query = """
-    SELECT d.document_id, d.file_name, u.email, d.archived, d.created_at, d.modified_at, ss.name, GROUP_CONCAT(a.name SEPARATOR ', ') 
+    SELECT d.document_id, d.file_name, u.email, d.archived, d.created_at, d.modified_at, ss.name AS subcategories, GROUP_CONCAT(a.name SEPARATOR ', ') AS aircrafts
     FROM documents AS d
     JOIN users AS u ON d.uploaded_by_id = u.user_id
     JOIN subcategories AS ss ON ss.subcategory_id = d.subcategory_id
-    JOIN aircraft_documents AS ad ON ad.documents_id = d.document_id
-    JOIN aircrafts AS a ON ad.aircrafts_id = a.aircraft_id
+    LEFT OUTER JOIN aircraft_documents AS ad ON ad.documents_id = d.document_id
+    LEFT OUTER JOIN aircrafts AS a ON ad.aircrafts_id = a.aircraft_id
     """
 
     filters = []
@@ -75,13 +88,15 @@ def build_query(parameters):
     if filters:
         query += " WHERE " + " AND ".join(filters)
     
+    query += " GROUP BY d.document_id "
+
     if 'columnName' in parameters:
         order = 'ASC' if parameters["asc"] == 'true' else 'DESC'
         query += f" ORDER BY d.{parameters["columnName"]} {order}"
     
     query += " LIMIT %s OFFSET %s"
-    limit = parameters["limit"]
-    offset = parameters["offset"]
+    limit = int(parameters["limit"])
+    offset = int(parameters["offset"])
     params.extend([limit, offset])
     return query, params
 
@@ -92,3 +107,9 @@ def connect_to_db():
         password=os.environ.get('PASSWORD'),
         database="adsats_database"
     )
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        return super().default(obj)
